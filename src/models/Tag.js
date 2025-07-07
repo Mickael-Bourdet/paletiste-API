0459e5b87c00937fa5693bf44dc67167ed84c7f0
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-07-07 16:26:46 +0200

    feat : create tag model

---
+++ src/models/Tag.js
@@ -0,0 +1,14 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "./client-sequelize.js";
+
+export class Tag extends Model {}
+
+Tag.init({
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  sequelize,
+  tableName: "tag",
+});
