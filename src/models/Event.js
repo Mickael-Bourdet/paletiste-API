74813c489730c080b48566fa92998502a078c751
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-07-07 16:41:21 +0200

    feat : create event model

---
+++ src/models/Event.js
@@ -0,0 +1,62 @@
+import sequelize from "./client-sequelize.js";
+import { DataTypes, Model } from "sequelize";
+
+export class Event extends Model {}
+
+Event.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    organizer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    poster: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    registration_time: {
+      type: DataTypes.TIME,
+      allowNull: true,
+    },
+    start_time: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    reservation: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    credit_card: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: "user",
+  }
+);
