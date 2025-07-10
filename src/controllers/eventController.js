d3de6336fb6c931a8e38b347bae13d8f30181adc
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-07-10 16:10:44 +0200

    feat : add first function in event controller

---
+++ src/controllers/eventController.js
@@ -0,0 +1,3 @@
+export const eventController = {
+  async getAllEvents(req, res, next) {},
+};
--- src/routers/eventRouter.js
+++ src/routers/eventRouter.js
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { eventController } from "../controllers/eventController.js";
 
 export const eventRouter = Router();
+
+eventRouter.get("/events", eventController.getAllEvents);
