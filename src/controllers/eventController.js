0fa0ec3244300edb7adba69f2a8476d7028d4fc3
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-08-06 16:38:23 +0200

    feat : get only useful information in getAllEvents

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -3,7 +3,24 @@ import { Event } from "../models/associations.js";
 export const eventController = {
   async getAllEvents(req, res, next) {
     try {
-      const events = await Event.findAll();
+      const events = await Event.findAll({
+        attributes: { exclude: ["category_id", "user_id"] }, //unnecessary fields
+        include: [
+          {
+            association: "category",
+            attributes: ["id", "name"],
+          },
+          {
+            association: "tags",
+            attributes: ["id", "name"],
+            through: { attributes: [] }, // don't return event_has_tag
+          },
+          {
+            association: "author",
+            attributes: ["id", "pseudo"],
+          },
+        ],
+      });
       res.status(200).json(events);
     } catch (error) {
       next(error);
