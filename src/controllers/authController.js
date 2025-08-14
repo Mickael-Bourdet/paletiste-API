c69fd2f133f047081d89f538c9075ff9cafda385
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-08-14 15:21:51 +0200

    feat : create every register useful files

---
+++ src/controllers/authController.js
@@ -0,0 +1,11 @@
+export const authController = {
+  /**
+   * Controller method to register a new user.
+   *
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   * @param {Function} next - The next middleware function.
+   * @returns {Object} - The response object with the registration status and user data.
+   */
+  async register(req, res, next) {},
+};
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,3 +1,13 @@
 import { Router } from "express";
+import { authController } from "../controllers/authController.js";
+import { catchAsync } from "../middlewares/catchAsync.js";
+import { validate } from "../middlewares/validationMiddleware.js";
+import { registerSchema } from "../middlewares/joiValidationSchema/registerSchema.js";
 
 export const authRouter = Router();
+
+authRouter.post(
+  "/register",
+  validate(registerSchema),
+  catchAsync(authController.register)
+);
